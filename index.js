e8e9a6712fd0dc7c5620f8bf9a8e1febcad4ad5d
+ <ronoh.ivy@gmail.com>
2022-06-04 06:00:33 +0300

    commit tree 80cd90e2ef7ad25ac159584d147b867a414abeb3

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(x,y){
+    return x-y;
+
+}
+
+function multiply(j,s){
+    return j*s;
+
+}
+
+function divide(juu,chini){
+return juu/chini;
+}
+
+number = 15;
+ function increment(n){
+     n ++;
+     return n;
+ }
+
+ function decrement(n){
+    n --;
+    return n;
+}
+
+function makeInt(n){
+return parseInt(n,10)
+
+}
+
+function preserveDecimal(n){
+    return parseFloat(n,10)
+    
+    }
